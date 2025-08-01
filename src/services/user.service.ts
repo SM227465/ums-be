ce2bc8c50dc353ed674d6f7ca39ecbe28c13272e
Sourav Mandal <sm227465@gmail.com>
2025-08-01 22:29:51 +0530

    small fix

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -195,7 +195,7 @@ export class UserService {
     }
 
     const parents = await User.find(query)
-      .select('firstName lastName email role')
+      .select('firstName lastName email role _id')
       .sort({ firstName: 1 });
 
     return parents;
