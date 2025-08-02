6a88f713e0dce4898e924343ebd8d47ef22abbdd
Sourav Mandal <sm227465@gmail.com>
2025-08-02 08:43:41 +0530

    remove rate limiter

--- src/app.ts
+++ src/app.ts
@@ -24,12 +24,12 @@ app.use(
   })
 );
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many request from your IP, please try again in an hour.',
-});
-app.use('/api', limiter);
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: 'Too many request from your IP, please try again in an hour.',
+// });
+// app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
